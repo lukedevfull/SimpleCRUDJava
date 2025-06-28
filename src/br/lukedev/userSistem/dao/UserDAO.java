d89668c214fab50bcfd62d4c822e2b592190c0a8
Lukedevfull <contato.prof2@gmail.com>
2025-06-28 19:22:37 -0300

    Refactor UserModel and UserDAO classes and created a saveUser method

--- src/br/lukedev/userSistem/dao/UserDAO.java
+++ src/br/lukedev/userSistem/dao/UserDAO.java
@@ -1,4 +1,18 @@
 package br.lukedev.userSistem.dao;
 
+import br.lukedev.userSistem.model.UserModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserDAO {
+    private final List<UserModel>  UsersModel = new ArrayList<>();
+
+    private long nextId = 1L;
+
+    public UserModel saveUser ( final UserModel user){
+        user.setId(nextId++);
+        UsersModel.add(user);
+        return user;
+    }
 }
--- src/br/lukedev/userSistem/model/UserModel.java
+++ src/br/lukedev/userSistem/model/UserModel.java
@@ -1,4 +1,68 @@
 package br.lukedev.userSistem.model;
 
-public record UserModel() {
+import java.time.OffsetDateTime;
+import java.util.Objects;
+
+public class UserModel {
+  private Long id;
+  private String name;
+  private String email;
+  private OffsetDateTime birthDate;
+
+    public UserModel(Long id, String name, String email, OffsetDateTime birthDate) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.birthDate = birthDate;
+    }
+
+    public UserModel() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public OffsetDateTime getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(OffsetDateTime birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        UserModel userModel = (UserModel) o;
+        return Objects.equals(id, userModel.id)
+                && Objects.equals(name, userModel.name)
+                && Objects.equals(email, userModel.email)
+                && Objects.equals(birthDate, userModel.birthDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email, birthDate);
+    }
 }
